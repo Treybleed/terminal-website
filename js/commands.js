6948cad28c74c3551350606b6520b77abc79b398
Matt Hobbs <matthew.hobbs@profound.works>
2023-04-11 09:55:37 +0100

    init

---
+++ js/commands.js
@@ -0,0 +1,56 @@
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
+  "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
+  "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
+  "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
+  "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
+  "         ...|-|\\--,\\_....                                                         /____/    © 2022",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<br/>',
+  '<span class="color2">Hello and welcome to my web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<br/>"
+];
